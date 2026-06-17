49106333b9a76fd2e137fc8576aa435b56767c01
LuisCF09 <rm99645@estudante.fieb.edu.br>
2026-06-17 14:58:57 +0000

    Criação do projeto spring boot

---
+++ zyclos/src/main/java/com/itb/inf2fm/zyclos/ZyclosApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.zyclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyclosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyclosApplication.class, args);
+	}
+
+}
---
+++ zyclos/src/test/java/com/itb/inf2fm/zyclos/ZyclosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.zyclos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyclosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
